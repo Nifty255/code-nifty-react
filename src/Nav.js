10b5dca6806e7d64ed18012b7248029299d47839
Nifty255 <nifty@codenifty.io>
2024-06-25 12:09:09 -0700

    Nav theming.

--- src/Nav.js
+++ src/Nav.js
@@ -5,10 +5,19 @@ import Routes from './Routes';
 
 import "./Nav.css";
 
+const customTheme = {
+  "link": {
+    "active": {
+      "on": "bg-orange-700 text-white dark:text-white md:bg-transparent md:text-orange-700 md:dark:text-orange-500",
+      "off": "border-b border-neutral-100 text-neutral-700 hover:bg-neutral-50 dark:border-neutral-700 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-cyan-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+    }
+  }
+};
+
 export default function Nav() {
   const { pathname } = useLocation();
   return (
-    <Navbar fluid className="w-full dark:bg-neutral-900">
+    <Navbar fluid theme={customTheme} className="w-full dark:bg-neutral-900">
       <Navbar.Brand as={Link} href="/">
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Code <span className="rainbow">{"{N}"}</span>ifty</span>
       </Navbar.Brand>
